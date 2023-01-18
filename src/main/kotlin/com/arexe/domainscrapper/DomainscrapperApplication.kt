dc23dd1b9411061228e8e8b4b26b9a25c5dafa5f
arexe <arexepro@gmail.com>
2023-01-18 18:58:20 +0100

    Initialize project

---
+++ src/main/kotlin/com/arexe/domainscrapper/DomainscrapperApplication.kt
@@ -0,0 +1,11 @@
+package com.arexe.domainscrapper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DomainscrapperApplication
+
+fun main(args: Array<String>) {
+	runApplication<DomainscrapperApplication>(*args)
+}
---
+++ src/test/kotlin/com/arexe/domainscrapper/DomainscrapperApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.arexe.domainscrapper
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DomainscrapperApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
